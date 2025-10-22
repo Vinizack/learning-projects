a42900078db50413d16b39e37f28098c70dcf609
ViniciusRibeiro <130787497+Vinizack@users.noreply.github.com>
2025-10-22 15:09:40 -0300

    Correção de erros

--- localSatelite/main.py
+++ localSatelite/main.py
@@ -4,17 +4,17 @@ from datetime import datetime
 my_lat = -23.956288
 my_lng = -46.326460
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-#
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-#
-# iss_postion = (longitude, latitude)
-#
-# print(iss_postion)
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+atitude = data["iss_position"]["latitude"]
+
+iss_postion = (longitude, latitude)
+
+print(iss_postion)
 
 parametros = {
     "lat": my_lat,
